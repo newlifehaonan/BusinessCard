b97900000c8352a946b4a7acfe94ee43c7c5fd1a
HJW <harry0520520@gmail.com>
2020-03-18 07:37:06 -0500

    delete key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,16 +7,16 @@ export const environment = {
 };
 
 export const config = {
-  apiKey: 'AIzaSyDDMDBKwkjyJ3xjnCbXTev9DL5GLqXgdio',
-  authDomain: 'businesscard-115bf.firebaseapp.com',
-  databaseURL: 'https://businesscard-115bf.firebaseio.com',
-  projectId: 'businesscard-115bf',
-  storageBucket: 'businesscard-115bf.appspot.com',
-  messagingSenderId: '983736355920'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 };
 
 export const googleApi = {
-  key : 'AIzaSyATp67RaDUZOT_isWP2NJH03r0dEw7DQws'
+  key : ''
 }
 
 /*
